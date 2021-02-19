8bd30ef28a845a8e06eb71160cfcfae9463e6eb9
Steve Rolfe <steve.rolfe@gmail.com>
2021-02-19 14:47:29 -0500

    CSV support

--- sl_run_sql.js
+++ sl_run_sql.js
@@ -6,7 +6,14 @@
  * @NScriptType Suitelet
  * @NApiVersion 2.x
  */
-define(['N/query', 'N/ui/serverWidget', 'N/log'], function(query, serverWidget, log) {
+define(['N/query', 'N/ui/serverWidget', 'N/log', 'N/file'], function(query, serverWidget, log, file) {
+	/** Which folder ID should CSVs be saved in */
+	var file_folder = 5851640;
+	
+	
+	/**
+	 * Execute a query and return results
+	 */
 	function runQuery(sql, limitRows) {
 		var response = { columns: [], rows: [], error: undefined };
 		if (limitRows != undefined && limitRows > 0) sql = 'SELECT * FROM (' + sql + ') WHERE ROWNUM <= ' + limitRows;
@@ -17,6 +24,8 @@ define(['N/query', 'N/ui/serverWidget', 'N/log'], function(query, serverWidget, 
 			response.error = e.message;
 			return response;
 		}
+		
+		log.debug('query returned', result.results.length + ' results');
 		
 		for (var i = 0; i < result.results.length; i++) {
 			if (response.columns.length == 0) {
@@ -47,43 +56,76 @@ define(['N/query', 'N/ui/serverWidget', 'N/log'], function(query, serverWidget, 
 				label: 'Limit Rows'
 			});
 			
+			// Output selection
+			var output_type = form.addField({
+				id: 'custpage_output_selector',
+				type: serverWidget.FieldType.SELECT,
+				label: 'Output Type'
+			});
+			output_type.addSelectOption({ value: 'grid', text: 'Grid' });
+			output_type.addSelectOption({ value: 'csv', text: 'CSV' });
+			
 			if (context.request.method === 'POST'){
 				// Log and run query
 				log.debug('Query', context.request.parameters.custpage_sql_field);
 				sql_field.defaultValue = context.request.parameters.custpage_sql_field;
 				limit_rows.defaultValue = context.request.parameters.custpage_limit_field;
+				output_type.defaultValue = context.request.parameters.custpage_output_selector;
 				var result = runQuery(context.request.parameters.custpage_sql_field, context.request.parameters.custpage_limit_field);
 				
 				if (result.rows.length > 0) {
-					// Output result
-					var result_list = form.addSublist({
-						id: 'sublist',
-						type: serverWidget.SublistType.LIST,
-						label: 'Results'
-					});
 					
-					// Headers
-					for (var ii = 0; ii < result.columns.length; ii++) {
-						result_list.addField({
-							id: 'custpage_' + ii + '_field',
-							label: result.columns[ii],
-							type: serverWidget.FieldType.TEXTAREA
+					if (context.request.parameters.custpage_output_selector == 'grid') {
+						// Output result on page
+						var result_list = form.addSublist({
+							id: 'sublist',
+							type: serverWidget.SublistType.LIST,
+							label: 'Results'
 						});
-					}
 					
-					// Data
-					var sublist = form.getSublist({ id: 'sublist' });
-					for (var row_num = 0; row_num < result.rows.length; row_num++) {
-						for (var col_num = 0; col_num < result.rows[row_num].length; col_num++) {
-							var row_col_value = result.rows[row_num][col_num];
-							if (row_col_value != null && row_col_value.length > 4000) row_col_value = row_col_value.substr(0, 3996) + '...';
-							sublist.setSublistValue({
-								id: 'custpage_' + col_num + '_field',
-								line: row_num,
-								value: row_col_value
+						// Headers
+						for (var ii = 0; ii < result.columns.length; ii++) {
+							result_list.addField({
+								id: 'custpage_' + ii + '_field',
+								label: result.columns[ii],
+								type: serverWidget.FieldType.TEXTAREA
 							});
 						}
+					
+						// Data
+						var sublist = form.getSublist({ id: 'sublist' });
+						for (var row_num = 0; row_num < result.rows.length; row_num++) {
+							for (var col_num = 0; col_num < result.rows[row_num].length; col_num++) {
+								var row_col_value = result.rows[row_num][col_num];
+								if (row_col_value != null && row_col_value.length > 4000) row_col_value = row_col_value.substr(0, 3996) + '...';
+								sublist.setSublistValue({
+									id: 'custpage_' + col_num + '_field',
+									line: row_num,
+									value: row_col_value
+								});
+							}
+						}
+					} else if (context.request.parameters.custpage_output_selector == 'csv') {
+						// Output to CSV file
+						var csv_file = file.create({
+							name: Date.now() + '.csv',
+							contents: result.columns.join(','),
+							folder: file_folder,
+							fileType: 'CSV'
+						});
+						for (var row_num = 0; row_num < result.rows.length; row_num++) {
+							csv_file.appendLine({ value: result.rows[row_num]/*.map(function(column){ return column.toString().replace(/\"/g, "\"\""); })*/.join(',') });
+						}
+						var file_id = csv_file.save();
+						form.addField({
+							id: 'custpage_csv_link',
+							type: serverWidget.FieldType.URL,
+							label: 'Download CSV'
+						}).updateDisplayType({
+							displayType: serverWidget.FieldDisplayType.INLINE
+						}).defaultValue = file.load({ id: file_id }).url;
 					}
+					
 				} else {
 					// Show error
 					var error_message;
